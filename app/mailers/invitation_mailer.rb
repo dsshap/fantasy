937820510c05ae344ec51c214cda2301bdaab727
Dave Shapiro <dss.shapiro@gmail.com>
2012-12-01 18:34:46 -0600

    fixing emailer

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,16 +1,16 @@
 class InvitationMailer < ActionMailer::Base
 
-  default :from => "Old Town Sports<team@oldtownsports.com>"
-  default :reply_to => "pinkyandthebrain@oldtownsports.com"
+  default :from => "Old Town Sports<team@oldtownsports.com>",
+          :reply_to => "pinkyandthebrain@oldtownsports.com"
 
-  def invite(inv)
-    inv.email, inv.fantasy_league.name, inv.join_league_link
-    @league_name = inv.fantasy_league.name
-    @email = inv.email
-    @link = inv.join_league_link
-    @inviter_email = inv.inviter_email
+  def invite(email, name, join_league_link, inviter_email)
+
+    @league_name = name
+    @email = email
+    @link = join_league_link
+    @inviter_email = inviter_email
     mail(:to => email,
-         :subject => "Old Town Sports Invitation to Fantasy League: #{league_name}")
+         :subject => "Old Town Sports Invitation to Fantasy League: #{@league_name}")
   end
 
 end
